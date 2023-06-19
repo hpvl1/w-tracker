b749ca75ce990c31c316eb6bd0710943c97d311f
Pavel <pvlwrk@gmail.com>
2023-06-19 17:02:33 +0300

    add router to main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
 import App from './App.vue';
+import router from './router';
 
 import './assets/styles/common.sass';
 
@@ -9,5 +10,6 @@ import './assets/styles/common.sass';
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(router);
 
 app.mount('#app');
