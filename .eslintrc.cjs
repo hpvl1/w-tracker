30b1a48f805abd485852986fefbd8679cb4164f6
Pavel <pvlwrk@gmail.com>
2023-06-23 15:17:18 +0300

    add eslint config file

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:vue/vue3-strongly-recommended'
+  ],
+  'overrides': [
+    {
+      'env': {
+        'node': true
+      },
+      'files': [
+        '.eslintrc.{js,cjs}'
+      ],
+      'parserOptions': {
+        'sourceType': 'script'
+      }
+    }
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'vue'
+  ],
+  'rules': {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+    indent: ['error', 2],
+    'comma-spacing': ['error', { before: false, after: true }],
+    'vue/no-multi-spaces': 0,
+  }
+};
