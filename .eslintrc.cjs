1134beac5e406ae66b40948ef8926c81f7db3678
Pavel <pvlwrk@gmail.com>
2023-06-23 15:56:00 +0300

    update and add new rule - max-attributes-per-line

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,37 +1,38 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true
+  env: {
+    browser: true,
+    es2021: true,
   },
-  'extends': [
-    'eslint:recommended',
-    'plugin:vue/vue3-strongly-recommended'
-  ],
-  'overrides': [
+  extends: ['eslint:recommended', 'plugin:vue/vue3-strongly-recommended'],
+  overrides: [
     {
-      'env': {
-        'node': true
+      env: {
+        node: true,
       },
-      'files': [
-        '.eslintrc.{js,cjs}'
-      ],
-      'parserOptions': {
-        'sourceType': 'script'
-      }
-    }
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
   ],
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module'
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  'plugins': [
-    'vue'
-  ],
-  'rules': {
+  plugins: ['vue'],
+  rules: {
     semi: ['error', 'always'],
     quotes: ['error', 'single'],
     indent: ['error', 2],
     'comma-spacing': ['error', { before: false, after: true }],
     'vue/no-multi-spaces': 0,
-  }
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: {
+          max: 3,
+        },
+      },
+    ],
+  },
 };
